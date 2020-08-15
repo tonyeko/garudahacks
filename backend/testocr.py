bff4c968185520db561e67bafced2b036efe3a08
tony <tonyeko@users.noreply.github.com>
2020-08-16 00:36:18 +0700

    add testcase

--- backend/testocr.py
+++ backend/testocr.py
@@ -2,7 +2,7 @@ from PIL import Image
 from pytesseract import image_to_string
 import re
 
-test = image_to_string(Image.open('./images/tc1.jpg'))
+test = image_to_string(Image.open('./images/tc3.jpg'))
 print(list(filter(lambda x: re.match(r"[a-zA-Z]+", x), test.split('\n'))))
 
 # Install tesseract: https://stackoverflow.com/questions/50655738/how-do-i-resolve-a-tesseractnotfounderror
