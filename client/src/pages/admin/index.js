cd66942d6c88f98c2f489168e1c2197d1241ce26
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2020-08-16 00:31:51 +0700

    Fetched medicine list

--- client/src/pages/admin/index.js
+++ client/src/pages/admin/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
+import axios from "axios";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -8,6 +9,20 @@ import MedicineDetails from "../../components/admin/MedicineDetails";
 
 const AdminIndexPage = () => {
   const [show, setShow] = useState(false);
+  const [medicines, setMedicines] = useState([]);
+  const [selectedMed, setSelectedMed] = useState({});
+
+  const getMedicines = useCallback(async () => {
+    await axios.get("http://localhost:5000/prescription").then((response) => {
+      const data = response.data;
+      console.log(data);
+      setMedicines(data);
+    });
+  }, [setMedicines]);
+
+  useEffect(() => {
+    getMedicines();
+  }, [getMedicines]);
 
   const testingCol = () => {
     let arr = [];
@@ -24,51 +39,46 @@ const AdminIndexPage = () => {
   return (
     <Container>
       <Alert variant="info">Remember to stock up medicines!</Alert>
-      <div className="position-relative">
-        <h1>Current Medicine Statistics</h1>
-        <div className="position-absolute bg-info" />
-        <Table responsive="lg">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Medicine Name</th>
-              <th>Stock</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Array.from({ length: 10 }).map((_, index) => {
-              return (
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>
-                    Paracetamol Mark {index + 1} Version v0.
-                    {Math.floor(Math.random() * 10) + 1}.
-                    {Math.floor(Math.random() * 15) + 1}
-                  </td>
-                  <td>{Math.floor(Math.random() * 100)}</td>
-                  <td>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setShow(true);
-                      }}
-                    >
-                      Details...
-                    </a>
-                  </td>
-                </tr>
-              );
-            })}
-            <MedicineDetails
-              show={show}
-              onHide={() => {
-                setShow(false);
-              }}
-            />
-          </tbody>
-        </Table>
-      </div>
+      <h1>Current Medicine Statistics</h1>
+      <Table responsive="lg">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Medicine Name</th>
+            <th>Stock</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {medicines.map((value, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{value.name}</td>
+                <td>{value.stock}</td>
+                <td>
+                  <a
+                    href="#"
+                    onClick={() => {
+                      setSelectedMed(value);
+                      setShow(true);
+                    }}
+                  >
+                    Details...
+                  </a>
+                </td>
+              </tr>
+            );
+          })}
+          <MedicineDetails
+            show={show}
+            onHide={() => {
+              setShow(false);
+            }}
+            selected={selectedMed}
+          />
+        </tbody>
+      </Table>
 
       <Row>{testingCol()}</Row>
     </Container>
